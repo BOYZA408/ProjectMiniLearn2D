fa80719da8c94a09c330c2f716ce8fecbf1bb7d9
XFACON <BOYZA408@hotmail.com>
2025-02-28 18:02:14 +0700

    update

--- main.gd
+++ main.gd
@@ -15,11 +15,14 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout() -> void:
@@ -47,6 +50,8 @@ func _on_mob_timer_timeout() -> void:
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+	
+	$HUD.update_score(score)
 
 
 func _on_score_timer_timeout() -> void:
